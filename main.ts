6498d4f9c9da39faaa6e37e41958865a79481319
Projects55 <aimasakovwork5@gmail.com>
2026-01-22 21:49:33 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -944,7 +944,7 @@ serve(async (req) => {
           await clearState(userId);
           return new Response("ok");
         } else {
-          const defaultTemplate = "```\n<happcode>\n```";
+          const defaultTemplate = "```<happcode>```";
           user.channels.push({
             chatId: chChatId,
             username,
